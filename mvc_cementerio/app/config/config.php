5a22489eb516ecc1ca38a40d17b5f8944e698704
EmiPlazza <81131241+EmiPlazza@users.noreply.github.com>
2025-08-05 10:44:41 -0300

    ahora

--- mvc_cementerio/app/config/config.php
+++ mvc_cementerio/app/config/config.php
@@ -1,11 +1,19 @@
 <?php
     define('APP', dirname(dirname(__FILE__)));
-    define('URL', 'http://localhost/cementerio/mvc_cementerio');
 
     
     define('DB_HOST', 'localhost');
     define('DB_NAME', 'cementerio');
     define('DB_USER', 'root');
     define('DB_PASS', '');
+    
+    // define('URL', 'http://localhost/cementerio/mvc_cementerio');
+
+    $protocolo = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https://' : 'http://';
+    $host = $_SERVER['HTTP_HOST'];
+    $direccionBase = '/cementerio/mvc_cementerio';
+
+    define('URL', $protocolo . $host . $direccionBase);
+
 
 ?>
