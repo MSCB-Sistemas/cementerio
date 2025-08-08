a828fee3e70d4a19d2b3002b82acf2f49dc37b57
Daniela Colque Chura <colque_daniela@hotmail.com>
2025-08-08 09:58:58 -0300

    update config.php y header.php de Home

--- mvc_cementerio/app/views/pages/home/HomeView.php
+++ mvc_cementerio/app/views/pages/home/HomeView.php
@@ -2,19 +2,8 @@
 
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema Cementerio</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="<?= URL ?>/public/css/styles.css">
-    <!-- Favicon -->
-    <link rel="icon" href="<?= URL ?>/public/img/favicon-16x16.png" type="image/x-icon">
-</head>
+<!-- Header -->
+    <?php require_once APP . '/views/pages/home/Footer.php'; ?>
 <body>
     <!-- Logo -->
     <div class="text-left my-1">
